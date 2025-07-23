040f3180eb72fd121db61e75e7926745e875a85e
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-07-23 09:35:10 -0300

    refactor(backend): renomeia classe principal e ajusta estrutura de pacotes para alunos

--- backend/src/main/java/br/com/rodartenogueira/alunos/AlunosApplication.java
+++ backend/src/main/java/br/com/rodartenogueira/alunos/AlunosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoApplication {
+public class AlunoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoApplication.class, args);
+		AlunoApplication.run(AlunoApplication.class, args);
 	}
 
 }
