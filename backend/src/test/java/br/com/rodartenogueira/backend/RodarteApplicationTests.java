e68273fc9f19827336ba12c617f7a84037f4e2a3
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-07-23 14:48:06 -0300

    test: adiciona teste da aplicação

--- backend/src/test/java/br/com/rodartenogueira/backend/RodarteApplicationTests.java
+++ backend/src/test/java/br/com/rodartenogueira/backend/RodarteApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.rodartenogueira.projeto;
+package br.com.rodartenogueira.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetoApplicationTests {
+class RodarteApplicationTests {
 
 	@Test
 	void contextLoads() {
