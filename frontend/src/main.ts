6999f7501f15e0552b791b90a075895eaca59a43
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-07-24 14:54:19 -0300

    chore(core): ajusta main.ts para bootstrap standalone (remove AppModule, inclui providers de rotas e interceptors)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-bootstrapApplication(AppModule, appConfig)
-  .catch((err) => console.error(err));
+import { LoadingInterceptor } from './app/core/interceptors/loading.interceptor';
+import { ErrorInterceptor } from './app/core/interceptors/error.interceptor';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ]
+}).catch(err => console.error(err));
